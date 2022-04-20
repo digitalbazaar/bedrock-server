2bd78775d4df097499d3ec47de6cb9e9e6b2ca9a
Dave Longley <dlongley@digitalbazaar.com>
2022-04-20 16:47:22 -0400

    Fix imports.

--- test/mocha/20-helpers.js
+++ test/mocha/20-helpers.js
@@ -1,17 +1,18 @@
 /*!
  * Copyright (c) 2014-2022 Digital Bazaar, Inc. All rights reserved.
  */
-'use strict';
+import {_parseCerts} from '@bedrock/server';
+import {fileURLToPath} from 'url';
+import path from 'path';
 
-const server = require('bedrock-server');
-const path = require('path');
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const mockCertBundle = path.join(__dirname, './mock.bundle.crt');
 
 describe('bedrock-server helpers', function() {
   describe('_parseCerts', function() {
     it('properly parses a cert bundle', async () => {
       const caFiles = mockCertBundle;
-      const parsedCerts = server._helpers._parseCerts({caFiles});
+      const parsedCerts = _parseCerts({caFiles});
       parsedCerts.should.be.an('array');
       parsedCerts.length.should.equal(2);
     });
